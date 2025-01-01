e7dfacb9134f1e6103064cf4e9281f35aead73f9
Muhammad Muneeb <80894553+Mmunib059@users.noreply.github.com>
2025-01-01 12:14:18 -0800

    Landing page

---
+++ Geely/script.js
@@ -0,0 +1,49 @@
+const barIcon = document.getElementById('bar-icon');
+const closeIcon = document.querySelector('.close-icon'); // Select by class
+const sidebar = document.getElementById('sidebar');
+
+barIcon.addEventListener('click', () => {
+  sidebar.classList.add('open');
+});
+
+closeIcon.addEventListener('click', () => {
+  sidebar.classList.remove('open');
+});
+
+
+// slides
+const menuBtn = document.querySelector(".menu-btn");
+const navigation = document.querySelector(".navigation");
+
+menuBtn.addEventListener("click", () => {
+    menuBtn.classList.toggle("active");
+    navigation.classList.toggle("active");
+});
+
+const btns = document.querySelectorAll(".nav-btn");
+const slides = document.querySelectorAll(".img-slide");
+const contents = document.querySelectorAll(".content");
+
+var sliderNav = function(manual){
+    btns.forEach((btn) => {
+        btn.classList.remove("active");
+    });
+
+    slides.forEach((slide) => {
+        slide.classList.remove("active");
+    });
+
+    contents.forEach((content) => {
+        content.classList.remove("active");
+    });
+
+    btns[manual].classList.add("active");
+    slides[manual].classList.add("active");
+    contents[manual].classList.add("active");
+}
+
+    btns.forEach((btn, i) => {
+        btn.addEventListener("click", () => {
+            sliderNav(i)
+        });
+    });
